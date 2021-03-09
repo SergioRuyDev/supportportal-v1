62e88f086b80f8089af79959bc1493512338d39f
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-09 17:28:21 +0400

    User details created.

---
+++ src/main/java/com/sergioruy/supportportal/domain/User.java
@@ -0,0 +1,22 @@
+package com.sergioruy.supportportal.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class User implements Serializable {
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate;
+    private String[] roles; // ROLE_USER{ (authority) read, edit }, ROLE_ADMIN { (authority) delete }
+    private String[] authorities;
+    private boolean isActive;
+    private boolean isNotLocked;
+}
