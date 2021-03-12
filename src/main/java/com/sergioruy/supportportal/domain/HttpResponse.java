6de06326fcee2895de9cae63e372b58001a20227
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-12 15:52:02 +0400

    Cleaning boilerplate with project lombok.

--- src/main/java/com/sergioruy/supportportal/domain/HttpResponse.java
+++ src/main/java/com/sergioruy/supportportal/domain/HttpResponse.java
@@ -1,52 +1,19 @@
 package com.sergioruy.supportportal.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 // This class is to get the responses as a Json with custom message.
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class HttpResponse {
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;
     private String message;
 
-    public HttpResponse() {}
-
-    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
-        this.httpStatusCode = httpStatusCode;
-        this.httpStatus = httpStatus;
-        this.reason = reason;
-        this.message = message;
-    }
-
-    public int getHttpStatusCode() {
-        return httpStatusCode;
-    }
-
-    public void setHttpStatusCode(int httpStatusCode) {
-        this.httpStatusCode = httpStatusCode;
-    }
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus;
-    }
-
-    public void setHttpStatus(HttpStatus httpStatus) {
-        this.httpStatus = httpStatus;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
