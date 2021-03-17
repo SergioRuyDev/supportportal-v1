8374654706a2f675c14a52458df552c61e01788b
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-17 16:16:00 +0400

    Authenticator Entrypoint in progress.

--- src/main/java/com/sergioruy/supportportal/filter/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/sergioruy/supportportal/filter/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package com.sergioruy.supportportal.filter;
 
+import com.sergioruy.supportportal.domain.HttpResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 
@@ -7,10 +8,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static com.sergioruy.supportportal.constant.SecurityConstant.*;
+import static org.springframework.http.HttpStatus.*;
+
 public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
 
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
             throws IOException {
+        HttpResponse httpResponse = new HttpResponse(
+                FORBIDDEN.value(), FORBIDDEN, FORBIDDEN.getReasonPhrase().toUpperCase(),
+                FORBIDDEN_MESSAGE);
+
         response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
     }
 }
