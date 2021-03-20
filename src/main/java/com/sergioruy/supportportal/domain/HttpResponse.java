3ff29b95ad0477267387d2396e921f1fa2b0604e
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-20 17:29:44 +0400

    Json format added to response.

--- src/main/java/com/sergioruy/supportportal/domain/HttpResponse.java
+++ src/main/java/com/sergioruy/supportportal/domain/HttpResponse.java
@@ -1,9 +1,12 @@
 package com.sergioruy.supportportal.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
+
+import java.util.Date;
 
 // This class is to get the responses as a Json with custom message.
 
@@ -11,9 +14,19 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 @NoArgsConstructor
 public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Asia/Dubai")
+    private Date timeStamp;
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;
     private String message;
+
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeStamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
 
 }
