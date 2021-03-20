ae0cea0e54583235dbe093ba2ddd725fc00f97fb
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-20 16:12:35 +0400

    Security Config in progress.

--- src/main/java/com/sergioruy/supportportal/configuration/SecurityConfiguration.java
+++ src/main/java/com/sergioruy/supportportal/configuration/SecurityConfiguration.java
@@ -1,16 +1,20 @@
 package com.sergioruy.supportportal.configuration;
 
+import com.sergioruy.supportportal.constant.SecurityConstant;
 import com.sergioruy.supportportal.filter.JwtAccessDeniedHandler;
 import com.sergioruy.supportportal.filter.JwtAuthenticationEntryPoint;
 import com.sergioruy.supportportal.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -23,11 +27,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.csrf().disable().cors().and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and().authorizeRequests().antMatchers(SecurityConstant.PUBLIC_URLS).permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .exceptionHandling().accessDeniedHandler(jwtAccessDeniedHandler)
+        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+        .and()
+        .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
