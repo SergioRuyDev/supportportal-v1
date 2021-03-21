9454ceb9ab41d11bf8ae9ba1b4f21b4f2ca46c07
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-21 15:54:24 +0400

    Exception Handling in progress.

--- src/main/java/com/sergioruy/supportportal/resource/UserResource.java
+++ src/main/java/com/sergioruy/supportportal/resource/UserResource.java
@@ -1,12 +1,13 @@
 package com.sergioruy.supportportal.resource;
 
+import com.sergioruy.supportportal.exception.domain.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/user")
-public class UserResource {
+public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
     public String showUser() {
