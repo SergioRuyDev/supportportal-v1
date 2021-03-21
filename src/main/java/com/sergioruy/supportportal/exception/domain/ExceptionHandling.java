fd5b446879498dc1bc3add85a6262bd01941488e
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-21 16:58:07 +0400

    Exception Handling in progress.

--- src/main/java/com/sergioruy/supportportal/exception/domain/ExceptionHandling.java
+++ src/main/java/com/sergioruy/supportportal/exception/domain/ExceptionHandling.java
@@ -1,18 +1,25 @@
 package com.sergioruy.supportportal.exception.domain;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.sergioruy.supportportal.domain.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.server.MethodNotAllowedException;
 
 import javax.security.auth.login.AccountLockedException;
 import java.nio.file.AccessDeniedException;
+import java.util.Objects;
+
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class ExceptionHandling {
@@ -26,43 +33,66 @@ public class ExceptionHandling {
     private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file.";
     private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission.";
 
-    @ExceptionHandler(AccountLockedException.class)
-    public ResponseEntity<HttpResponse> accountLockedException() {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, ACCOUNT_LOCKED);
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<HttpResponse> accountDisabledException() {
+        return createHttpResponse(BAD_REQUEST, ACCOUNT_DISABLED);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     public ResponseEntity<HttpResponse> badCredentialsException() {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, INCORRECT_CREDENTIALS);
+        return createHttpResponse(BAD_REQUEST, INCORRECT_CREDENTIALS);
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<HttpResponse> accessDeniedException() {
-        return createHttpResponse(HttpStatus.FORBIDDEN, NOT_ENOUGH_PERMISSION);
+        return createHttpResponse(FORBIDDEN, NOT_ENOUGH_PERMISSION);
     }
 
-    @ExceptionHandler(MethodNotAllowedException.class)
-    public ResponseEntity<HttpResponse> MethoException() {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, ACCOUNT_DISABLED);
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<HttpResponse> lockedException() {
+        return createHttpResponse(UNAUTHORIZED, ACCOUNT_LOCKED);
     }
 
-    @ExceptionHandler(DisabledException.class)
-    public ResponseEntity<HttpResponse> accountDisabledException() {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, ACCOUNT_DISABLED);
+    @ExceptionHandler(TokenExpiredException.class)
+    public ResponseEntity<HttpResponse> tokenExpiredException(TokenExpiredException exception) {
+        return createHttpResponse(UNAUTHORIZED, exception.getMessage());
     }
 
-    @ExceptionHandler(DisabledException.class)
-    public ResponseEntity<HttpResponse> accountDisabledException() {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, ACCOUNT_DISABLED);
+    @ExceptionHandler(EmailExistException.class)
+    public ResponseEntity<HttpResponse> emailExistException(EmailExistException exception) {
+        return createHttpResponse(BAD_REQUEST, exception.getMessage());
     }
 
-    @ExceptionHandler(DisabledException.class)
-    public ResponseEntity<HttpResponse> accountDisabledException() {
-        return createHttpResponse(HttpStatus.BAD_REQUEST, ACCOUNT_DISABLED);
+    @ExceptionHandler(UsernameExistException.class)
+    public ResponseEntity<HttpResponse> usernameExistException(UsernameExistException exception) {
+        return createHttpResponse(BAD_REQUEST, exception.getMessage());
     }
+
+    @ExceptionHandler(EmailNotFoundException.class)
+    public ResponseEntity<HttpResponse> emailNotFoundException(EmailNotFoundException exception) {
+        return createHttpResponse(BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<HttpResponse> userNotFoundException(UserNotFoundException exception) {
+        return createHttpResponse(BAD_REQUEST, exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<HttpResponse> methodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
+        HttpMethod supportedMethod = Objects.requireNonNull(exception.getSupportedHttpMethods()).iterator().next();
+        return createHttpResponse(METHOD_NOT_ALLOWED, String.format(METHOD_IS_NOT_ALLOWED, supportedMethod));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<HttpResponse> internalServerErrorException(Exception exception) {
+        return createHttpResponse(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_MSG);
+    }
+
 
     private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message) {
         return new ResponseEntity<>(new HttpResponse(httpStatus.value(), httpStatus, httpStatus.getReasonPhrase().toUpperCase()
                 , message.toUpperCase()), httpStatus);
     }
+
 }
