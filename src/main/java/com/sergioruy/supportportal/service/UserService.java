cdba088bbeaf2ac490d470b75b1549a942382d93
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-23 17:47:22 +0400

    Register in progress.

--- src/main/java/com/sergioruy/supportportal/service/UserService.java
+++ src/main/java/com/sergioruy/supportportal/service/UserService.java
@@ -1,4 +1,18 @@
 package com.sergioruy.supportportal.service;
 
+import com.sergioruy.supportportal.domain.User;
+import com.sergioruy.supportportal.exception.domain.EmailExistException;
+import com.sergioruy.supportportal.exception.domain.UsernameExistException;
+
+import java.util.List;
+
 public interface UserService {
+
+    User register(String firstName, String lastName, String username, String email) throws UsernameExistException, EmailExistException;
+
+    List<User> getUsers();
+
+    User findByUsername(String username);
+
+    User findUserByEmail(String email);
 }
--- src/main/java/com/sergioruy/supportportal/service/impl/UserServiceImpl.java
+++ src/main/java/com/sergioruy/supportportal/service/impl/UserServiceImpl.java
@@ -2,8 +2,12 @@ package com.sergioruy.supportportal.service.impl;
 
 import com.sergioruy.supportportal.domain.User;
 import com.sergioruy.supportportal.domain.UserPrincipal;
+import com.sergioruy.supportportal.exception.domain.EmailExistException;
+import com.sergioruy.supportportal.exception.domain.EmailNotFoundException;
+import com.sergioruy.supportportal.exception.domain.UsernameExistException;
 import com.sergioruy.supportportal.repository.UserRepository;
 import com.sergioruy.supportportal.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.Date;
+import java.util.List;
 
 @Service
 @Transactional
@@ -43,5 +48,56 @@ public class UserServiceImpl implements UserService, UserDetailsService {
             LOGGER.info("Returning found user by username: " + username);
             return null;
         }
+    }
+
+    @Override
+    public User register(String firstName, String lastName, String username, String email) throws UsernameExistException, EmailExistException {
+        validateNewUsernameAndEmail(StringUtils.EMPTY, username, email);
+        return null;
+    }
+
+    private User validateNewUsernameAndEmail(String currentUsername, String newUsername, String newEmail) throws UsernameExistException, EmailExistException {
+        if (StringUtils.isNotBlank(currentUsername)) {
+            User currentUser = findByUsername(currentUsername);
+            if (currentUser == null) {
+                throw new UsernameNotFoundException("No user found by username " + currentUsername);
+            }
+            User userByUsername = findByUsername(newUsername);
+            if (userByUsername != null && !currentUser.getId().equals(userByUsername.getId())) {
+                throw new UsernameExistException("Username already exists.");
+            }
+
+            User userByEmail = findUserByEmail(newEmail);
+            if (userByEmail != null && !currentUser.getId().equals(userByEmail.getId())) {
+                throw new EmailExistException("Email already exists.");
+            }
+            return currentUser;
+        } else {
+            User userByUsername = findByUsername(newUsername);
+            if (userByUsername != null) {
+                throw new UsernameExistException("Username already exists.");
+            }
+            User userByEmail = findUserByEmail(newEmail);
+            if (userByEmail != null) {
+                throw new EmailExistException("Email already exists.");
+            }
+            return null;
+
+        }
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return null;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        return null;
     }
 }
