9dba347181432700d69b0fbcebd150d570b78db6
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-23 16:54:10 +0400

    White label custom exception done.

--- src/main/java/com/sergioruy/supportportal/exception/domain/ExceptionHandling.java
+++ src/main/java/com/sergioruy/supportportal/exception/domain/ExceptionHandling.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.sergioruy.supportportal.domain.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
@@ -23,7 +25,7 @@ import java.util.Objects;
 import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
-public class ExceptionHandling {
+public class ExceptionHandling implements ErrorController {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     private static final String ACCOUNT_LOCKED = "Your account has been locked. Please contact administration.";
@@ -33,6 +35,7 @@ public class ExceptionHandling {
     private static final String ACCOUNT_DISABLED = "Your account has been disabled. Please contact the administration.";
     private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file.";
     private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission.";
+    public static final String ERROR_PATH = "/error";
 
     @ExceptionHandler(DisabledException.class)
     public ResponseEntity<HttpResponse> accountDisabledException() {
@@ -79,10 +82,10 @@ public class ExceptionHandling {
         return createHttpResponse(BAD_REQUEST, exception.getMessage());
     }
 
-    @ExceptionHandler(NoHandlerFoundException.class)
+/*    @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<HttpResponse> methodNotSupportedException(NoHandlerFoundException e) {
         return createHttpResponse(BAD_REQUEST, "This page was not found.");
-    }
+    }*/
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<HttpResponse> methodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
@@ -114,4 +117,13 @@ public class ExceptionHandling {
                 , message.toUpperCase()), httpStatus);
     }
 
+    @RequestMapping(ERROR_PATH)
+    public ResponseEntity<HttpResponse> notFound404() {
+        return createHttpResponse(NOT_FOUND, "There is no mapping for this URL.");
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
 }
