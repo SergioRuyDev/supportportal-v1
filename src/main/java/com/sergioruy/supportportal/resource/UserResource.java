8a9820ef23da047c0125ea95e29de546156830c2
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-26 10:05:49 +0400

    Register in progress.

--- src/main/java/com/sergioruy/supportportal/resource/UserResource.java
+++ src/main/java/com/sergioruy/supportportal/resource/UserResource.java
@@ -1,18 +1,29 @@
 package com.sergioruy.supportportal.resource;
 
+import com.sergioruy.supportportal.domain.User;
+import com.sergioruy.supportportal.exception.domain.EmailExistException;
 import com.sergioruy.supportportal.exception.domain.ExceptionHandling;
 import com.sergioruy.supportportal.exception.domain.UserNotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.sergioruy.supportportal.exception.domain.UsernameExistException;
+import com.sergioruy.supportportal.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = {"/", "/user"})
 public class UserResource extends ExceptionHandling {
+    private UserService userService;
 
-    @GetMapping("/home")
-    public String showUser() throws UserNotFoundException {
-        //return "application works";
-        throw new UserNotFoundException("The user was not found.");
+    @Autowired
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User user) throws UserNotFoundException, UsernameExistException, EmailExistException {
+       User newUser = userService.register(user.getFirstName(), user.getLastName(), user.getUsername(), user.getEmail());
+       return new ResponseEntity<>(newUser, HttpStatus.OK);
     }
 }
