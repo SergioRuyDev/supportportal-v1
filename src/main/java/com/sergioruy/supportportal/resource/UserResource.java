93cf3954e64be1032a2ca464dd7d020d8e88230d
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-27 10:55:38 +0400

    Logging JWT generating in progress.

--- src/main/java/com/sergioruy/supportportal/resource/UserResource.java
+++ src/main/java/com/sergioruy/supportportal/resource/UserResource.java
@@ -1,12 +1,14 @@
 package com.sergioruy.supportportal.resource;
 
 import com.sergioruy.supportportal.domain.User;
+import com.sergioruy.supportportal.domain.UserPrincipal;
 import com.sergioruy.supportportal.exception.domain.EmailExistException;
 import com.sergioruy.supportportal.exception.domain.ExceptionHandling;
 import com.sergioruy.supportportal.exception.domain.UserNotFoundException;
 import com.sergioruy.supportportal.exception.domain.UsernameExistException;
 import com.sergioruy.supportportal.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,6 +22,18 @@ public class UserResource extends ExceptionHandling {
     @Autowired
     public UserResource(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> register(@RequestBody User user) {
+        authenticate(user.getUsername(), user.getPassword());
+        User loginUser = userService.findByUsername(user.getUsername());
+        UserPrincipal userPrincipal = new UserPrincipal(loginUser);
+        HttpHeaders jwtHeader = getJwtHeader(userPrincipal);
+        return new ResponseEntity<>(loginUser, OK);
+    }
+
+    private void authenticate(String username, String password) {
     }
 
     @PostMapping("/register")
