58443a296a48de901227adf831bbcea22895ece9
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-29 10:35:09 +0400

    Authenticatio Failure done.

---
+++ src/main/java/com/sergioruy/supportportal/listener/AuthenticationFailureListener.java
@@ -0,0 +1,27 @@
+package com.sergioruy.supportportal.listener;
+
+import com.sergioruy.supportportal.service.LoginAttemptService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class AuthenticationFailureListener {
+    private LoginAttemptService loginAttemptService;
+
+
+    @EventListener
+    public void onAuthenticationFailure(AuthenticationFailureBadCredentialsEvent event) throws ExecutionException {
+        Object principal = event.getAuthentication().getPrincipal();
+        if (principal instanceof String) {
+            String username = (String) event.getAuthentication().getPrincipal();
+            loginAttemptService.addUserToLoginAttemptCache(username);
+        }
+    }
+}
