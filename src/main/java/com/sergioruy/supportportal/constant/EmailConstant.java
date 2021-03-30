9594663039690636baa6ee7efccf75335a8cfa93
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-30 10:44:09 +0400

    Email notification in progress.

--- src/main/java/com/sergioruy/supportportal/constant/EmailConstant.java
+++ src/main/java/com/sergioruy/supportportal/constant/EmailConstant.java
@@ -9,9 +9,9 @@ public class EmailConstant {
     public static final String EMAIL_SUBJECT = "Sergio Ruy Dev, LLC";
     public static final String GMAIL_SMTP_SERVER = "smtp.gmail.com";
     public static final String SMTP_HOST = "mail.smtp.host";
-    public static final String SMTP_HOST = "mail.smtp.host";
-    public static final String SMTP_HOST = "mail.smtp.host";
-    public static final String SMTP_HOST = "mail.smtp.host";
-    public static final String SMTP_HOST = "mail.smtp.host";
-    public static final String SMTP_HOST = "mail.smtp.host";
+    public static final String SMTP_AUTH = "mail.smtp.auth";
+    public static final String SMTP_PORT = "mail.smtp.port";
+    public static final int DEFAULT_PORT = 465;
+    public static final String SMTP_STARTTLS_ENABLE = "mail.smtp.starttls.enable";
+    public static final String SMTP_STARTTLS_REQUIRED = "mail.smtp.starttls.required";
 }
