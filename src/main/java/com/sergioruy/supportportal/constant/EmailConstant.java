3b2da187d3160bb07d9dc8609e9f3a2beebb03f2
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-30 11:15:29 +0400

    Email notification in progress.

--- src/main/java/com/sergioruy/supportportal/constant/EmailConstant.java
+++ src/main/java/com/sergioruy/supportportal/constant/EmailConstant.java
@@ -2,8 +2,8 @@ package com.sergioruy.supportportal.constant;
 
 public class EmailConstant {
     public static final String SIMPLE_MAIL_TRANSFER_PROTOCOL = "smtps";
-    public static final String USERNAME = "email@gmail.com";
-    public static final String PASSWORD = "your password";
+    public static final String USERNAME = "sergioruyenator@gmail.com";
+    public static final String PASSWORD = "Julia917";
     public static final String FROM_EMAIL = "support@sergioruydev.com";
     public static final String CC_EMAIL = "";
     public static final String EMAIL_SUBJECT = "Sergio Ruy Dev, LLC - New Password";
