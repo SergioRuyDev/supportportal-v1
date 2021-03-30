118d75901cc630faeb5e2e3e5e3412978ac4f948
SergioRuyDev <sergioruyenator@gmail.com>
2021-03-30 11:07:59 +0400

    Email notification in progress.

--- src/main/java/com/sergioruy/supportportal/service/EmailService.java
+++ src/main/java/com/sergioruy/supportportal/service/EmailService.java
@@ -2,13 +2,30 @@ package com.sergioruy.supportportal.service;
 
 import org.springframework.stereotype.Service;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Date;
 import java.util.Properties;
 
 import static com.sergioruy.supportportal.constant.EmailConstant.*;
 
 @Service
 public class EmailService {
+
+    private Message createEmail(String firstName, String password, String email) throws MessagingException {
+        Message message = new MimeMessage(getEmailSession());
+        message.setFrom(new InternetAddress(FROM_EMAIL));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email, false));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(CC_EMAIL, false));
+        message.setSubject(EMAIL_SUBJECT);
+        message.setText("Hello " + firstName + ", \n \n Your nem account password is: " + password + "\n \n The Support Team.");
+        message.setSentDate(new Date());
+        message.saveChanges();
+        return message;
+    }
 
     private Session getEmailSession() {
         Properties properties = System.getProperties();
@@ -17,5 +34,6 @@ public class EmailService {
         properties.put(SMTP_PORT, DEFAULT_PORT);
         properties.put(SMTP_STARTTLS_ENABLE, true);
         properties.put(SMTP_STARTTLS_REQUIRED, true);
+        return Session.getInstance(properties, null);
     }
 }
