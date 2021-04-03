70f0050298de4eaf57bf5f72940bddc3a071d95b
SergioRuyDev <sergioruyenator@gmail.com>
2021-04-03 15:39:46 +0400

    Design Resource implementation done.

--- src/main/java/com/sergioruy/supportportal/resource/UserResource.java
+++ src/main/java/com/sergioruy/supportportal/resource/UserResource.java
@@ -18,13 +18,15 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-import static com.sergioruy.supportportal.constant.FileConstant.FORWARD_SLASH;
-import static com.sergioruy.supportportal.constant.FileConstant.USER_FOLDER;
+import static com.sergioruy.supportportal.constant.FileConstant.*;
 import static com.sergioruy.supportportal.constant.SecurityConstant.JWT_TOKEN_HEADER;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
@@ -129,7 +131,20 @@ public class UserResource extends ExceptionHandling {
         return Files.readAllBytes(Paths.get(USER_FOLDER + username + FORWARD_SLASH + filename));
     }
 
-    @GetMapping()
+    @GetMapping(path = "/image/{profile}/{username}", produces = IMAGE_JPEG_VALUE)
+    public byte[] getTempProfileImage(@PathVariable("username") String username) throws IOException {
+        URL url = new URL(TEMP_PROFILE_IMAGE_BASE_URL + username);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try (InputStream inputStream = url.openStream()) {
+            int bytesRead;
+            byte[] chunk = new byte[1024];
+            while ((bytesRead = inputStream.read(chunk)) > 0) {
+                byteArrayOutputStream.write(chunk, 0, bytesRead);
+            }
+        }
+        return byteArrayOutputStream.toByteArray();
+    }
+
 
     private ResponseEntity<HttpResponse> response(HttpStatus httpStatus, String message) {
         return new ResponseEntity<>(new HttpResponse(httpStatus.value(), httpStatus, httpStatus.getReasonPhrase().toUpperCase(),
