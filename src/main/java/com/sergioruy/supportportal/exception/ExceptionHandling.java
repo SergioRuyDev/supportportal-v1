5640addc570ead25d6eb521a30b7e18bdc60c2db
SergioRuyDev <sergioruyenator@gmail.com>
2021-04-07 16:20:20 +0400

    Small refactor.

--- src/main/java/com/sergioruy/supportportal/exception/ExceptionHandling.java
+++ src/main/java/com/sergioruy/supportportal/exception/ExceptionHandling.java
@@ -2,16 +2,14 @@ package com.sergioruy.supportportal.exception;
 
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.sergioruy.supportportal.domain.HttpResponse;
-import com.sergioruy.supportportal.exception.domain.EmailExistException;
-import com.sergioruy.supportportal.exception.domain.EmailNotFoundException;
-import com.sergioruy.supportportal.exception.domain.UserNotFoundException;
-import com.sergioruy.supportportal.exception.domain.UsernameExistException;
+import com.sergioruy.supportportal.exception.domain.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
@@ -22,22 +20,21 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.persistence.NoResultException;
 import java.io.IOException;
-import java.nio.file.AccessDeniedException;
 import java.util.Objects;
 
 import static org.springframework.http.HttpStatus.*;
 
+
 @RestControllerAdvice
 public class ExceptionHandling implements ErrorController {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
-
-    private static final String ACCOUNT_LOCKED = "Your account has been locked. Please contact administration.";
-    private static final String METHOD_IS_NOT_ALLOWED = "This request method is not allowed on this endpoint. Please send a '%s' request.";
+    private static final String ACCOUNT_LOCKED = "Your account has been locked. Please contact administration";
+    private static final String METHOD_IS_NOT_ALLOWED = "This request method is not allowed on this endpoint. Please send a '%s' request";
     private static final String INTERNAL_SERVER_ERROR_MSG = "An error occurred while processing the request";
-    private static final String INCORRECT_CREDENTIALS = "Username / password incorrect. Please try again.";
-    private static final String ACCOUNT_DISABLED = "Your account has been disabled. Please contact the administration.";
-    private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file.";
-    private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission.";
+    private static final String INCORRECT_CREDENTIALS = "Username / password incorrect. Please try again";
+    private static final String ACCOUNT_DISABLED = "Your account has been disabled. If this is an error, please contact administration";
+    private static final String ERROR_PROCESSING_FILE = "Error occurred while processing file";
+    private static final String NOT_ENOUGH_PERMISSION = "You do not have enough permission";
     public static final String ERROR_PATH = "/error";
 
     @ExceptionHandler(DisabledException.class)
@@ -85,10 +82,10 @@ public class ExceptionHandling implements ErrorController {
         return createHttpResponse(BAD_REQUEST, exception.getMessage());
     }
 
-/*    @ExceptionHandler(NoHandlerFoundException.class)
-    public ResponseEntity<HttpResponse> methodNotSupportedException(NoHandlerFoundException e) {
-        return createHttpResponse(BAD_REQUEST, "This page was not found.");
-    }*/
+//    @ExceptionHandler(NoHandlerFoundException.class)
+//    public ResponseEntity<HttpResponse> noHandlerFoundException(NoHandlerFoundException e) {
+//        return createHttpResponse(BAD_REQUEST, "There is no mapping for this URL");
+//    }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<HttpResponse> methodNotSupportedException(HttpRequestMethodNotSupportedException exception) {
@@ -100,6 +97,12 @@ public class ExceptionHandling implements ErrorController {
     public ResponseEntity<HttpResponse> internalServerErrorException(Exception exception) {
         LOGGER.error(exception.getMessage());
         return createHttpResponse(INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_MSG);
+    }
+
+    @ExceptionHandler(NotAnImageFileException.class)
+    public ResponseEntity<HttpResponse> notAnImageFileException(NotAnImageFileException exception) {
+        LOGGER.error(exception.getMessage());
+        return createHttpResponse(BAD_REQUEST, exception.getMessage());
     }
 
     @ExceptionHandler(NoResultException.class)
@@ -114,15 +117,14 @@ public class ExceptionHandling implements ErrorController {
         return createHttpResponse(INTERNAL_SERVER_ERROR, ERROR_PROCESSING_FILE);
     }
 
-
     private ResponseEntity<HttpResponse> createHttpResponse(HttpStatus httpStatus, String message) {
-        return new ResponseEntity<>(new HttpResponse(httpStatus.value(), httpStatus, httpStatus.getReasonPhrase().toUpperCase()
-                , message.toUpperCase()), httpStatus);
+        return new ResponseEntity<>(new HttpResponse(httpStatus.value(), httpStatus,
+                httpStatus.getReasonPhrase().toUpperCase(), message), httpStatus);
     }
 
     @RequestMapping(ERROR_PATH)
     public ResponseEntity<HttpResponse> notFound404() {
-        return createHttpResponse(NOT_FOUND, "There is no mapping for this URL.");
+        return createHttpResponse(NOT_FOUND, "There is no mapping for this URL");
     }
 
     @Override
