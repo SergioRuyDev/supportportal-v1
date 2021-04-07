a1d28335bf6799455913bb92c496e223d7ea446c
SergioRuyDev <sergioruyenator@gmail.com>
2021-04-07 16:23:37 +0400

    Small refactor.

--- src/main/java/com/sergioruy/supportportal/filter/JwtAccessDeniedHandler.java
+++ src/main/java/com/sergioruy/supportportal/filter/JwtAccessDeniedHandler.java
@@ -2,6 +2,7 @@ package com.sergioruy.supportportal.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sergioruy.supportportal.domain.HttpResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -13,20 +14,18 @@ import java.io.OutputStream;
 
 import static com.sergioruy.supportportal.constant.SecurityConstant.ACCESS_DENIED_MESSAGE;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
-import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
     @Override
-    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception)
-            throws IOException {
-            HttpResponse httpResponse = new HttpResponse(UNAUTHORIZED.value(), UNAUTHORIZED, UNAUTHORIZED.getReasonPhrase()
-                    .toUpperCase(), ACCESS_DENIED_MESSAGE);
-            response.setContentType(APPLICATION_JSON_VALUE);
-            response.setStatus(UNAUTHORIZED.value());
-            OutputStream outputStream = response.getOutputStream();
-            ObjectMapper mapper = new ObjectMapper();
-            mapper.writeValue(outputStream, httpResponse);
-            outputStream.flush();
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception) throws IOException {
+        HttpResponse httpResponse = new HttpResponse(UNAUTHORIZED.value(), UNAUTHORIZED, UNAUTHORIZED.getReasonPhrase().toUpperCase(), ACCESS_DENIED_MESSAGE);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(UNAUTHORIZED.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream, httpResponse);
+        outputStream.flush();
     }
 }
