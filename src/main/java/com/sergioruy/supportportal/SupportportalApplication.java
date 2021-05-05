29f9a4e9adc9cf7443257aa270ebabc908ec641d
SergioRuyDev <sergioruyenator@gmail.com>
2021-05-05 13:09:38 +0400

    Deployed.

--- src/main/java/com/sergioruy/supportportal/SupportportalApplication.java
+++ src/main/java/com/sergioruy/supportportal/SupportportalApplication.java
@@ -28,7 +28,7 @@ public class SupportportalApplication {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
 //        corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://3.141.194.13/"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", ""));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
                 "Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
                 "Access-Control-Request-Method", "Access-Control-Request-Headers"));
